ea6c1ec361732756d6d1cb6f347e9904f14afcca
c-128 <noemail@noemail>
2024-08-03 22:35:51 +0200

    move server files into server directory

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 mkdir -p build
+mkdir -p build/server
 rm -rf build/*
 
 for os in "linux" "windows" "darwin"; do
     for arch in "amd64" "arm" "arm64"; do
-        output="build/staticly-$os-$arch"
+        output="staticly-$os-$arch"
         if [[ $os == "windows" ]]; then
             output="$output.exe"
         fi
 
-        GOOS=$os GOARCH=$arch go build -o $output *.go
+        GOOS=$os GOARCH=$arch go build -o build/server/$output server/*.go
     done
 done
